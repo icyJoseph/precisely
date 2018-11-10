b95d0a045a4de64f504e95b2f7204be0764eb7b5
icyJoseph <sephxd1234@gmail.com>
2018-11-10 19:38:28 +0100

    feat: display customer and contract names! :tada:

--- src/containers/Contracts/index.js
+++ src/containers/Contracts/index.js
@@ -2,8 +2,17 @@ import React from "react";
 
 import withData from "../WithData";
 
-export function Contracts() {
-  return <h1 className="display-3">Contract</h1>;
+export function Contracts({ contracts, customers }) {
+  return (
+    <div>
+      <h1 className="display-3">Contracts</h1>
+      <ul style={{ listStyle: "none" }}>
+        {contracts.map(({ id, name }) => (
+          <li key={id}>{name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default withData(Contracts);
--- src/containers/Customers/index.js
+++ src/containers/Customers/index.js
@@ -2,8 +2,17 @@ import React from "react";
 
 import withData from "../WithData";
 
-export function Customers() {
-  return <h1 className="display-3">Customers</h1>;
+export function Customers({ contracts, customers }) {
+  return (
+    <div>
+      <h1 className="display-3">Customers</h1>
+      <ul style={{ listStyle: "none" }}>
+        {customers.map(({ id, name }) => (
+          <li key={id}>{name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default withData(Customers);
