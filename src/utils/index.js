1eb77f0498dad5d149a911e43ac8b32db259d9b4
icyJoseph <sephxd1234@gmail.com>
2018-11-10 13:40:26 +0100

    chore: add comments

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,12 @@
+// given a React Class Component
+// take the name of a piece of state
+// and toggle its value
 export function toggleState(key) {
   return this.setState(prev => ({ [key]: !prev[key] }));
 }
 
+// take a function
+// then take a first group of arguments
+// then take a second group of arguments
+// return the evaluation of the function with the first and second group
 export const partial = f => (...a) => (...b) => f(...a, ...b);
--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -1,4 +1,4 @@
-import { partial, toggleState } from "./";
+import { goTo, partial, toggleState } from "./";
 
 describe("toggleState", () => {
   const ctx = {
@@ -22,5 +22,14 @@ describe("partial", () => {
   it("redefine a new function, which makes use of add", () => {
     const add3 = partial(add)(3);
     expect(add3(4)).toEqual(7);
+  });
+});
+
+describe("goTo", () => {
+  const history = { push: jest.fn() };
+  const route = "/home";
+  it("calls push with the route", () => {
+    goTo(history, route);
+    expect(history.push).toHaveBeenCalledWith(route);
   });
 });
