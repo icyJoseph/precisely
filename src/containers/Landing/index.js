1e85b0eb435073e99c691a8fa2bcd1a0a9540ff7
icyJoseph <sephxd1234@gmail.com>
2018-11-10 13:21:36 +0100

    feat: navigation to contracts and customers

--- src/containers/Landing/index.js
+++ src/containers/Landing/index.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Button } from "reactstrap";
 
-export function Landing() {
-  return <h1 className="display-3">Landing</h1>;
+import { routes } from "../../constants";
+import { partial } from "../../utils";
+
+const goTo = (history, route) => history.push(route);
+
+function LinkButton({ callback, text }) {
+  return (
+    <Button color="primary" onClick={callback}>
+      {text}
+    </Button>
+  );
+}
+
+export function Landing({ history }) {
+  return (
+    <div>
+      <h1 className="display-3">Landing</h1>
+      {routes.slice(1).map(route => (
+        <LinkButton
+          key={route}
+          callback={partial(goTo)(history, `/${route}`)}
+          text={route}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Landing;
