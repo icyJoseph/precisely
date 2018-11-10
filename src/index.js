3ed4a8827bbe0cd00d335f3a1dd22e63e79a43ab
icyJoseph <sephxd1234@gmail.com>
2018-11-10 18:39:57 +0100

    feat: inject routes with provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import Routes from "./routes";
-import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
+import rootReducer from "./ducks";
+import Routes from "./routes";
+import { unregister } from "./serviceWorker";
+
+import "./index.css";
+
+// use redux dev tools
+const ReduxDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const initialState = { customers: [], contracts: [] };
+
+const store = createStore(rootReducer, initialState, ReduxDevTools);
+
+const app = (
+  <Provider store={store}>
+    <Routes />
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+unregister();
