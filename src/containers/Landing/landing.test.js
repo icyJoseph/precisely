1e894e6b208b3d45e5ee7bc2986fc913c55372f2
icyJoseph <sephxd1234@gmail.com>
2018-11-10 13:39:19 +0100

    test: expected behaviour described in challenge

--- src/containers/Landing/landing.test.js
+++ src/containers/Landing/landing.test.js
@@ -1,10 +1,25 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { Landing } from "./";
+import { LinkButton } from "../../components/LinkButton";
 
 describe("renders the landing container", () => {
+  const landing = shallow(<Landing />);
+  const buttons = landing.find(LinkButton);
+
   it("renders", () => {
-    const landing = shallow(<Landing />);
     expect(landing).toBeDefined();
+  });
+
+  it("renders two LinkButtons", () => {
+    expect(buttons).toHaveLength(2);
+  });
+
+  it("first button is for contracts", () => {
+    expect(buttons.at(0).prop("text")).toEqual("contracts");
+  });
+
+  it("second button is for customers", () => {
+    expect(buttons.at(1).prop("text")).toEqual("customers");
   });
 });
