47028d9e01b335a42204c96e10434dc33bb33ab5
icyJoseph <sephxd1234@gmail.com>
2018-11-10 23:39:43 +0100

    chore: comment data structure for customers/contracts

--- src/data/index.js
+++ src/data/index.js
@@ -1,4 +1,10 @@
 import customers from "./customers";
 import contracts from "./contracts";
 
+// Both data objects are Arrays,
+// which is common in data coming from API's
+// however it is not the most optimal when manipulating data
+// for a redux store
+// Ideally, this data should come formatted as an Object, which allows us to
+// modify and delete more easily
 export { customers, contracts };
