3c51fc3d8265f1ca6f41f3c4a56be3b1e34e89dd
icyJoseph <sephxd1234@gmail.com>
2018-11-11 01:08:11 +0100

    feat: show a card for every asset, customer or contract, as requested :tada:

--- src/containers/Contracts/index.js
+++ src/containers/Contracts/index.js
@@ -1,14 +1,31 @@
 import React from "react";
+import Card from "../../components/Card";
+import { content } from "../../constants";
+import { partial } from "../../utils";
+
+const tempAction = id => console.log("clicked ", id);
 
 export function Contracts({ contracts, customers }) {
   return (
     <div>
       <h1 className="display-3">Contracts</h1>
-      <ul style={{ listStyle: "none" }}>
-        {Object.keys(contracts).map(id => (
-          <li key={id}>{contracts[id].name}</li>
-        ))}
-      </ul>
+      <div>
+        {Object.keys(contracts).map(contractId => {
+          // get the meta data of the contract
+          const { name, customerId } = contracts[contractId];
+          const customer = customers[customerId].name;
+          const subtitle = `Made for ${customer}`;
+          return (
+            <Card
+              key={contractId}
+              title={name}
+              subtitle={subtitle}
+              content={content}
+              action={partial(tempAction)(contractId)}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/containers/Customers/index.js
+++ src/containers/Customers/index.js
@@ -1,14 +1,40 @@
 import React from "react";
+import Card from "../../components/Card";
+
+import { partial } from "../../utils";
+
+const tempAction = id => console.log("clicked ", id);
 
 export function Customers({ contracts, customers }) {
   return (
     <div>
       <h1 className="display-3">Customers</h1>
-      <ul style={{ listStyle: "none" }}>
-        {Object.keys(customers).map(id => (
-          <li key={id}>{customers[id].name}</li>
-        ))}
-      </ul>
+      <div>
+        {Object.keys(customers).map(customerId => {
+          // get customer meta data
+          const { name } = customers[customerId];
+          // get all contracts as content of the card
+          const content = Object.keys(contracts)
+            .filter(
+              contractId => contracts[contractId].customerId === customerId
+            )
+            .map(contractId => ({
+              id: contractId,
+              text: contracts[contractId].name
+            }));
+
+          return (
+            <Card
+              key={customerId}
+              title={name}
+              content={content}
+              action={partial(tempAction)(customerId)}
+              actionName="Delete"
+              buttonColor="danger"
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
