f1db40935973becc0337732de6de62fed83de50c
icyJoseph <sephxd1234@gmail.com>
2018-11-12 22:46:06 +0100

    feat: use a confirmation modal when clicking delete

--- src/containers/Customers/customers.test.js
+++ src/containers/Customers/customers.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { Customers } from "./";
+import Card from "../../components/Card";
+import Confirmation from "../../components/Confirmation";
 
 import { customers, contracts } from "../../data";
 import { arrayAsObjectById } from "../../utils";
@@ -9,10 +11,49 @@ const objCustomers = arrayAsObjectById(customers);
 const objContracts = arrayAsObjectById(contracts);
 
 describe("renders the contracts container", () => {
+  const container = shallow(
+    <Customers customers={objCustomers} contracts={objContracts} />
+  );
+
   it("renders", () => {
-    const container = shallow(
-      <Customers customers={objCustomers} contracts={objContracts} />
-    );
     expect(container).toBeDefined();
+  });
+
+  it("does not show confirmation Modal initially", () => {
+    expect(container.find(Confirmation)).toHaveLength(0);
+  });
+
+  it("changes Modal state when clicking a card", () => {
+    const [{ id }] = customers;
+    // get initial State
+    const initialState = container.state();
+    // call the action prop on Card
+    container
+      .find(Card)
+      .at(0)
+      .prop("action")();
+    // get next state
+    const nextState = container.state();
+    // compare
+    expect(initialState.showModal).toEqual(!nextState.showModal);
+    // verify that the selectedCustomerId is now the expected id
+    expect(nextState.selectedCustomerId).toEqual(id);
+  });
+
+  it("shows the confirmation modal", () => {
+    expect(container.find(Confirmation)).toHaveLength(1);
+  });
+
+  it("closes the modal and clears the state", () => {
+    container.find(Confirmation).prop("close")();
+    const finalState = container.state();
+    const {
+      showModal,
+      selectedCustomerId,
+      selectedCustomerContracts
+    } = finalState;
+    expect(showModal).toEqual(false);
+    expect(selectedCustomerId).toEqual("");
+    expect(selectedCustomerContracts).toEqual([]);
   });
 });
--- src/containers/Customers/index.js
+++ src/containers/Customers/index.js
@@ -1,44 +1,94 @@
-import React from "react";
+import React, { Component } from "react";
 import Card from "../../components/Card";
-import { CUSTOMERS } from "../../constants";
-import { partial } from "../../utils";
+import Confirmation from "../../components/Confirmation";
+import { CUSTOMERS, DELETE } from "../../constants";
+import { partial, toggleState } from "../../utils";
 
-export function Customers({ contracts, customers, deleteCustomer }) {
-  const customersIds = Object.keys(customers);
-  return (
-    <div>
-      <h1 className="display-3">
-        {CUSTOMERS} {`(${customersIds.length})`}
-      </h1>
-      <div className="card-grid">
-        {customersIds.map(customerId => {
-          // get customer meta data
-          const { name } = customers[customerId];
-          // get all contracts as content of the card
-          const content = Object.keys(contracts)
-            .filter(
-              contractId => contracts[contractId].customerId === customerId
-            )
-            .map(contractId => ({
-              id: contractId,
-              text: contracts[contractId].name
-            }));
+const initialState = {
+  showModal: false,
+  selectedCustomerId: "",
+  selectedCustomerContracts: []
+};
 
-          // get all contract ids for the current customer
-          return (
-            <Card
-              key={customerId}
-              title={name}
-              content={content}
-              action={partial(deleteCustomer)(customerId, content)}
-              actionName="Delete"
-              buttonColor="danger"
-            />
-          );
-        })}
+function toInitialState(initialState) {
+  return this.setState({ ...initialState });
+}
+
+export class Customers extends Component {
+  state = {
+    ...initialState
+  };
+
+  toggle = toggleState.bind(this, "showModal");
+
+  open = (selectedCustomerId, selectedCustomerContracts) =>
+    this.setState({
+      showModal: true,
+      selectedCustomerId,
+      selectedCustomerContracts
+    });
+
+  close = toInitialState.bind(this, initialState);
+
+  render() {
+    const { contracts, customers, deleteCustomer } = this.props;
+    const {
+      showModal,
+      selectedCustomerId,
+      selectedCustomerContracts
+    } = this.state;
+
+    const currentCustomer = selectedCustomerId && customers[selectedCustomerId];
+
+    const customersIds = Object.keys(customers);
+    return (
+      <div>
+        <h1 className="display-3">
+          {CUSTOMERS} {`(${customersIds.length})`}
+        </h1>
+        <div className="card-grid">
+          {customersIds.map(customerId => {
+            // get customer meta data
+            const { name } = customers[customerId];
+            // get all contracts as content of the card
+            const customerContracts = Object.keys(contracts)
+              .filter(
+                contractId => contracts[contractId].customerId === customerId
+              )
+              .map(contractId => ({
+                id: contractId,
+                text: contracts[contractId].name
+              }));
+
+            // get all contract ids for the current customer
+            return (
+              <Card
+                key={customerId}
+                title={name}
+                content={customerContracts}
+                action={partial(this.open)(customerId, customerContracts)}
+                actionName="Delete"
+                buttonColor="danger"
+              />
+            );
+          })}
+        </div>
+        {showModal && (
+          <Confirmation
+            customer={currentCustomer}
+            contracts={selectedCustomerContracts}
+            showModal={showModal}
+            action={deleteCustomer}
+            actionPayload={[selectedCustomerId, selectedCustomerContracts]}
+            toggle={this.toggle}
+            close={this.close}
+            afterAction={this.close}
+            actionName={DELETE}
+          />
+        )}
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Customers;
